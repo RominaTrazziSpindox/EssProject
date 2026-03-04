1f4e1a82bad013a2b13ca55891b246275f4aab15
Your Name <you@example.com>
2026-03-04 15:51:16 +0100

    [CHORES] added comments and improve function name

--- IngestionAPI/src/main/java/com/spx/messaging/CampaignPublisher.java
+++ IngestionAPI/src/main/java/com/spx/messaging/CampaignPublisher.java
@@ -1,11 +1,13 @@
 package com.spx.messaging;
 
+import com.spx.dto.CrmIncomingCampaignDTO;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CampaignPublisher {
 
+    // Constructor injection
     private final RabbitTemplate rabbitTemplate;
     private final RabbitConfigProperties rabbitConfigProperties;
 
@@ -14,8 +16,13 @@ public class CampaignPublisher {
         this.rabbitConfigProperties = rabbitConfigProperties;
     }
 
-    public void publishCampaignEvent(Object event) {
-        rabbitTemplate.convertAndSend(rabbitConfigProperties.exchange(), rabbitConfigProperties.routingKey(), event
+    /* Method that receives a single campaign at a time of DTO type, then convert it in JSON format and
+     send it to the configured exchange (TopicExchange) using the routing key defined in application.yaml. */
+    public void publishCampaign(CrmIncomingCampaignDTO singleCampaignDTO) {
+        rabbitTemplate.convertAndSend(
+                rabbitConfigProperties.exchange(),
+                rabbitConfigProperties.routingKey(),
+                singleCampaignDTO
         );
     }
 }
