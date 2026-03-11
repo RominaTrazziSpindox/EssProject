3c48c64a74f586163245c74dd88bf0ea98bb321d
Your Name <you@example.com>
2026-03-11 11:50:18 +0100

    [FIX]: added comments and improved CampaignMapper with @AfterMapping. This annotation links each Attendee to its parent Campaign, ensuring the JPA @ManyToOne side is populated so Hibernate can persist the campaign_id foreign key correctly.

--- EventWorker/src/main/java/com/spx/mappers/AttendeeMapper.java
+++ EventWorker/src/main/java/com/spx/mappers/AttendeeMapper.java
@@ -6,10 +6,22 @@ import org.mapstruct.Mapper;
 
 import java.util.List;
 
+/*
+ * MapStruct mapper responsible for converting AttendeeDTO objects
+ * into Attendee entities.
+ *
+ * This mapper is used by CampaignMapper when mapping the list of
+ * attendees contained in CampaignEventDTO.
+ */
 @Mapper(componentModel = "spring")
 public interface AttendeeMapper {
 
+    /*
+     * Converts a single AttendeeDTO into an Attendee entity.
+     */
     Attendee toEntity(AttendeeDTO dto);
+
+
     List<Attendee> toEntityList(List<AttendeeDTO> dtoList);
 
 }
