e86444c9b6ca18801e9c23cfd1cce00eadd17b51
Your Name <you@example.com>
2026-03-19 14:22:48 +0100

    [REFACTOR]: centralize and validate RabbitMQ configuration using @ConfigurationProperties in both microservices

--- IngestionAPI/src/main/java/com/spx/messaging/CampaignPublisher.java
+++ IngestionAPI/src/main/java/com/spx/messaging/CampaignPublisher.java
@@ -1,6 +1,6 @@
 package com.spx.messaging;
 
-import com.spx.config.RabbitConfigProperties;
+import com.spx.config.RabbitProperties;
 import com.spx.dto.CrmIncomingCampaignDTO;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
@@ -10,19 +10,19 @@ public class CampaignPublisher {
 
     // Constructor injection
     private final RabbitTemplate rabbitTemplate;
-    private final RabbitConfigProperties rabbitConfigProperties;
+    private final RabbitProperties rabbitProperties;
 
-    public CampaignPublisher(RabbitTemplate rabbitTemplate, RabbitConfigProperties rabbitConfigProperties) {
+    public CampaignPublisher(RabbitTemplate rabbitTemplate, RabbitProperties rabbitProperties) {
         this.rabbitTemplate = rabbitTemplate;
-        this.rabbitConfigProperties = rabbitConfigProperties;
+        this.rabbitProperties = rabbitProperties;
     }
 
     /* Method that receives a single campaign at a time of DTO type, then convert it in JSON format and
      send it to the configured exchange (TopicExchange) using the routing key defined in application.yaml. */
     public void publishCampaign(CrmIncomingCampaignDTO singleCampaignDTO) {
         rabbitTemplate.convertAndSend(
-                rabbitConfigProperties.exchange(),
-                rabbitConfigProperties.routingKey(),
+                rabbitProperties.getExchange(),
+                rabbitProperties.getRoutingKey(),
                 singleCampaignDTO
         );
     }
