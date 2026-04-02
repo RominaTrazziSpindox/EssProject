4a5b2052e48ca3f7043c6b16946c097d645bbaf5
Your Name <you@example.com>
2026-04-02 17:19:18 +0200

    [FEATURE]: Starting Navbar component

--- EssFrontend/src/App.jsx
+++ EssFrontend/src/App.jsx
@@ -1,14 +1,15 @@
 import BlueColumn from './components/BlueColumn.jsx'
-
+import Navbar from './components/Navbar.jsx'
 function App() {
-   
-
     return (
        
-        <div>
-            <BlueColumn />
-        </div>
+    <div className="page">
+      <BlueColumn />
 
+        <div className="main-area">
+            <Navbar />
+        </div>
+    </div>
 
     )
 }
---
+++ EssFrontend/src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+function Navbar() {
+
+
+  return (
+    <nav className="navbar">
+
+      <div>
+        <h1>Campaigns Generator</h1>
+      </div>
+
+      <ul className="navlinks">
+        <li>
+           <a href="http://localhost:3000">Home</a>
+        </li>
+
+        <li>
+          <a href="http://localhost:3000/report">Campaigns</a>
+        </li>
+
+        <li>
+          <a href="http://localhost:3000/report">Reports</a>
+        </li>
+
+        <li>
+          <a href="http://localhost:3000/analytics">Analytics</a>
+        </li>
+
+        <li>
+          <a href="http://localhost:3000/analytics">Support</a>
+        </li>
+      </ul>
+
+      <div className="person">
+        <i className="fa-regular fa-user"></i>
+        <span>Admin</span>
+          <a href="http://localhost:3000">
+            <i className="fa-solid fa-chevron-down"></i>
+          </a>
+      </div>
+      
+    </nav>
+  )
+}
+
+export default Navbar
