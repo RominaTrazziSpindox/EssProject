640fbf95f2c4e728e63fbea7ef518d8c46c7d528
Your Name <you@example.com>
2026-04-16 09:44:04 +0200

    [FEATURE]: added try-catch logic for email test.

--- EventWorker/src/main/java/com/spx/reporting/EmailTest.java
+++ EventWorker/src/main/java/com/spx/reporting/EmailTest.java
@@ -6,6 +6,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 // Runs an email test when the application starts
 @Component
 @Slf4j
@@ -21,8 +23,20 @@ public class EmailTest implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
+
         log.info("Starting email test.");
-        reportEmailService.sendTestEmail();
+
+        try {
+
+            // Send the email to MailHog server
+            reportEmailService.sendTestEmail();
+            log.info("Email sent successfully");
+
+        } catch (RuntimeException exception) {
+
+            log.error("Failed to send the email to the SMTP server.", exception);
+            throw new IllegalStateException("Unable to send the email.", exception);
+        }
     }
 }
 
